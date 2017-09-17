bfce23d832720805311395e9fd904d0987c4da2d
Sergei Zelinsky <sergei.zelinsky.dev@gmail.com>
2017-09-18 00:31:29 +0300

    index.ts: use ShapesStorage instead of simple array

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
-import Shape from './src/Shape';
 import Rectangle from './src/Rectangle';
 import Circle from './src/Circle';
+import ShapesStorage from './src/ShapesStorage';
 
-const shapes: Shape[] = [];
+const shapesStorage = new ShapesStorage();
 
-shapes.push(new Rectangle(4, 2));
-shapes.push(new Circle(42));
+shapesStorage.add(new Rectangle(4, 2));
+shapesStorage.add(new Circle(42));
 
-const totalArea = shapes.reduce((area: number, shape: Shape) => {
-    return area + shape.getArea();
-}, 0);
+const totalArea = shapesStorage.getTotalArea();
 
 console.log(`Total area: ${totalArea}`);
